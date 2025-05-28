5a80f99f9a0cec92fc51a974390bee94cdd60a84
adminTIIIPSY <deadkingofhearts80@gmail.com>
2025-05-27 22:07:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function githubLogin() {
+  alert('GitHub login will be connected soon.');
+}
+function resetPassword() {
+  alert('Password reset function coming soon.');
+}
+document.getElementById('loginForm')?.addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Login functionality will be added with Firebase Auth.');
+});
+document.getElementById('registerForm')?.addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Register functionality will be added with Firebase Auth.');
+});
